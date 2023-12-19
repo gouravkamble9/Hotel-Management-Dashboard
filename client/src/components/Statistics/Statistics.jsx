4ef8f40436e72ac06c74195c0ca1230e724db4cd
Gourav <gouravkamble9@gmail.com>
2023-12-19 21:01:40 +0530

    -

--- client/src/components/Statistics/Statistics.jsx
+++ client/src/components/Statistics/Statistics.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect} from "react";
 import StatisticsCard from "./StatisticsCard/StatisticsCard";
 import "./Statistics.scss";
 import { FaBed } from "react-icons/fa";
